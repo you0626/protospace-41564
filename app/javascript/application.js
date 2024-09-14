498e39fba03d0e6f652932fee17a4ce1bc75e1a8
you0626 <sy0626sk@icloud.com>
2024-09-14 22:46:20 +0900

    投稿できなかった時に画像が残らない仕様３

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  const form = document.querySelector('form');
+  
+  if (form) {
+    form.addEventListener('submit', () => {
+      const fileInput = document.querySelector('#prototype_image');
+      if (fileInput && form.querySelector('.field_with_errors')) {
+        fileInput.value = '';  // 画像フィールドをリセット
+      }
+    });
+  }
+});
