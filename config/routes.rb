cc339993f8c2c796f862478cb54b34c8a2bf84f9
you0626 <sy0626sk@icloud.com>
2024-09-15 17:49:48 +0900

    Revert "ログイン関係なく表示"
    This reverts commit 0a004cbe151ebdf9715342a8987b3db6ba1689f2.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: :show
   get 'prototypes/index'
-  root to: 'prototypes#index'
+  root to: "prototypes#index"
   resources :prototypes do
     resources :comments, only: [:create]
   end
