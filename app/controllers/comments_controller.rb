16f159b261035cbe367a13a07b474f5588b73a6f
you0626 <sy0626sk@icloud.com>
2024-09-15 16:05:54 +0900

    コメントのルーティング変更２

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      render "prototypes/show", status: :unprocessable_entity
+      render "prototypes/show"
     end
   end
   
