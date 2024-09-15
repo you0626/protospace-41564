90b2e3a7ab32497b5b1bd0144aab6492bbdb810f
you0626 <sy0626sk@icloud.com>
2024-09-15 16:21:12 +0900

    コメントのルーティング変更４

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      redirect_to action: "prototypes/show"
+      redirect_to prototype_path(@comment.prototype)
     end
   end
   
