decc3ed33770ddc7ecf4d41217633265fc06042b
you0626 <sy0626sk@icloud.com>
2024-09-15 15:31:21 +0900

    Revert "ログアウト機能修正"
    This reverts commit 1ab4689def3a8d0c31f5b487d02ff28d7c8ad3c9.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,4 @@ Rails.application.routes.draw do
   resources :prototypes do
     resources :comments, only: [:create]
   end
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-  
 end
